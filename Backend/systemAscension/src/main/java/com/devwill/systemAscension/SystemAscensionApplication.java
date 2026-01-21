6e1618a79286d03a9773140f81edf67caa2ab36c
willAtaides <sg552.santos@gmail.com>
2026-01-21 18:52:21 -0300

    project created

---
+++ Backend/systemAscension/src/main/java/com/devwill/systemAscension/SystemAscensionApplication.java
@@ -0,0 +1,13 @@
+package com.devwill.systemAscension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAscensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemAscensionApplication.class, args);
+	}
+
+}
---
+++ Backend/systemAscension/src/test/java/com/devwill/systemAscension/SystemAscensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwill.systemAscension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemAscensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
